805dc992ea76ac80b78d530f122315ab137d3aa9
samanta-scavassa <samanta.camargoterra@gmail.com>
2024-01-22 17:50:46 +0100

    create cat class

---
+++ js/cat.js
@@ -0,0 +1,42 @@
+class Cat {
+  constructor(gameScreen, left, top, width, height, image) {
+    this.gameScreen = gameScreen;
+    this.left = left;
+    this.top = top;
+    this.width = width;
+    this.height = height;
+    this.directionX = 0;
+    this.directionY = 0;
+    this.element = document.createElement("img");
+    this.element.style.position = "absolute";
+    this.element.style.height = `${this.height}px`;
+    this.element.style.width = `${this.width}px`;
+    this.element.style.top = `${this.top}px`;
+    this.element.style.left = `${this.left}px`;
+    this.gameScreen.appendChild(this.element);
+  }
+
+  move() {
+    const maxLeft = this.gameScreen.offsetWidth - this.width;
+    const maxTop = this.gameScreen.offsetHeight - this.height;
+
+    if (this.top < maxTop && this.directionY > 0) {
+      this.top += this.directionY;
+    } else if (this.top > 0 && this.directionY < 0) {
+      this.top += this.directionY;
+    } else if (this.left < maxLeft && this.directionX > 0) {
+      this.left += this.directionX;
+    } else if (this.left > 0 && this.directionX < 0) {
+      this.left += this.directionX;
+    }
+
+    this.updatePosition();
+  }
+
+  updatePosition() {
+    this.element.style.top = `${this.top}px`;
+    this.element.style.left = `${this.left}px`;
+    this.directionX = 0;
+    this.directionY = 0;
+  }
+}
