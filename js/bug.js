0a0eba0a51b8498b26c664d5748354dfbda28899
samanta-scavassa <samanta.camargoterra@gmail.com>
2024-01-25 14:37:18 +0100

    fix images and sound not found

--- js/bug.js
+++ js/bug.js
@@ -2,7 +2,7 @@ class Bug extends Eats {
   constructor(gameScreen) {
     super(gameScreen);
     this.element = document.createElement("img");
-    this.element.setAttribute("src", "../images/bug.png");
+    this.element.setAttribute("src", "./images/bug.png");
     this.element.style.position = "absolute";
     this.element.style.height = `${this.height}px`;
     this.element.style.width = `${this.width}px`;
@@ -10,7 +10,7 @@ class Bug extends Eats {
     this.element.style.left = `${this.left}px`;
     this.element.style.zIndex = 4;
     this.gameScreen.appendChild(this.element);
-    this.collectBugSound = new Audio("../sound/collect_bug.flac")
+    this.collectBugSound = new Audio("./sound/collect_bug.flac")
   }
 
   digest() {
