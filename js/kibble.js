1e44eb8998701d62e97eb133295eb8fe0535e7cf
samanta-scavassa <samanta.camargoterra@gmail.com>
2024-01-25 17:20:23 +0100

    test

--- js/kibble.js
+++ js/kibble.js
@@ -2,7 +2,7 @@ class Kibble extends Eats {
   constructor(gameScreen) {
     super(gameScreen);
     this.element = document.createElement("img");
-    this.element.setAttribute("src", "./images/food.png");
+    this.element.setAttribute("src", "/images/food.png");
     this.element.style.position = "absolute";
     this.element.style.height = `${this.height}px`;
     this.element.style.width = `${this.width}px`;
@@ -10,7 +10,7 @@ class Kibble extends Eats {
     this.element.style.left = `${this.left}px`;
     this.element.style.zIndex = 4;
     this.gameScreen.appendChild(this.element);
-    this.collectKibbleSound = new Audio("./sound/collect_food.wav");
+    this.collectKibbleSound = new Audio("/sound/collect_food.wav");
   }
 
   digest() {
